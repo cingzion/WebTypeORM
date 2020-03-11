fb70999c961ba31da0244226601904dd9110b522
jean <jean@live.con>
2020-03-11 17:42:38 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ createConnection().then(async connection => {
     console.log("Inserting a new user into the database...");
     const user = new User();
     user.firstName = "Timber";
-    user.lastName = "Saw";
+    user.lastName = "Saws";
     user.age = 25;
     await connection.manager.save(user);
     console.log("Saved a new user with id: " + user.id);
